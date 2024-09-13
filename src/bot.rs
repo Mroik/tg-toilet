8a6b7309d14739376aa9b7ec257499b91f8eaf90
Mroik <mroik@delayed.space>
2024-09-13 05:38:10 +0200

    Implement recording new shitting session

--- src/bot.rs
+++ src/bot.rs
@@ -1,11 +1,18 @@
-use crate::{database::create_or_update_user, BOT_NAME};
+use crate::{
+    database::{
+        create_or_update_user, insert_shitting_session, insert_shitting_session_with_duration,
+        insert_shitting_session_with_location,
+    },
+    BOT_NAME,
+};
 use log::error;
 use rusqlite::Connection;
 use std::sync::Arc;
 use teloxide::{
     macros::BotCommands,
-    prelude::ResponseResult,
-    types::{Message, User},
+    payloads::SendMessageSetters,
+    prelude::{Requester, ResponseResult},
+    types::{Message, ReplyParameters},
     utils::command::{parse_command, BotCommands as _},
     Bot,
 };
@@ -14,7 +21,7 @@ use tokio::sync::Mutex;
 #[derive(BotCommands)]
 #[command(rename_rule = "lowercase")]
 enum Command {
-    Cagando,
+    Shitting,
 }
 
 pub async fn answer(conn: Arc<Mutex<Connection>>, bot: Bot, msg: Message) -> ResponseResult<()> {
@@ -24,7 +31,7 @@ pub async fn answer(conn: Arc<Mutex<Connection>>, bot: Bot, msg: Message) -> Res
 
     let text = msg.text().unwrap();
     let command = Command::parse(text, BOT_NAME);
-    if command.is_err() || msg.from.is_none() {
+    if command.is_err() || msg.from.is_none() || !msg.chat.is_chat() {
         return Ok(());
     }
 
@@ -33,18 +40,86 @@ pub async fn answer(conn: Arc<Mutex<Connection>>, bot: Bot, msg: Message) -> Res
         .is_err()
     {
         error!("Failed to query number of users");
+        bot.send_message(
+            msg.chat.id,
+            "There was an error with the database 😥\n Please report this incident to @Mroik",
+        )
+        .reply_parameters(ReplyParameters::new(msg.id))
+        .await?;
         return Ok(());
     }
 
     match command.unwrap() {
-        Command::Cagando => answer_cagando(bot, text, msg.from.as_ref().unwrap()).await,
+        Command::Shitting => answer_shitting(conn, bot, msg).await,
     }
 }
 
-async fn answer_cagando(bot: Bot, text: &str, user: &User) -> ResponseResult<()> {
-    let (_, args) = parse_command(text, BOT_NAME).unwrap();
-    match args.len() {
-        _ => (), // TODO
+async fn answer_shitting(
+    conn: Arc<Mutex<Connection>>,
+    bot: Bot,
+    msg: Message,
+) -> ResponseResult<()> {
+    let (_, args) = parse_command(msg.text().unwrap(), BOT_NAME).unwrap();
+    let user = msg.from.as_ref().unwrap();
+
+    let failed = match args.len() {
+        0 => insert_shitting_session(conn, user, false, false)
+            .await
+            .is_err(),
+        1 => insert_shitting_session_with_duration(
+            conn,
+            user,
+            args.first().unwrap().parse().unwrap(),
+            false,
+            false,
+        )
+        .await
+        .is_err(),
+        2 => insert_shitting_session_with_location(
+            conn,
+            user,
+            args.first().unwrap().parse().unwrap(),
+            args.get(1).unwrap(),
+            false,
+            false,
+        )
+        .await
+        .is_err(),
+        3 => insert_shitting_session_with_location(
+            conn,
+            user,
+            args.first().unwrap().parse().unwrap(),
+            args.get(1).unwrap(),
+            args.get(2).unwrap().parse().unwrap(),
+            false,
+        )
+        .await
+        .is_err(),
+        4 => insert_shitting_session_with_location(
+            conn,
+            user,
+            args.first().unwrap().parse().unwrap(),
+            args.get(1).unwrap(),
+            args.get(2).unwrap().parse().unwrap(),
+            args.get(3).unwrap().parse().unwrap(),
+        )
+        .await
+        .is_err(),
+        _ => {
+            bot.send_message(msg.chat.id, "⚠️⚠️⚠️\nUsage:\n/shitting\n/shitting duration\n/shitting duration location\n/shitting duration location haemorrhoids\n/shitting duration location haemorrhoids constipated\nDuration: in seconds\nLocation: A string without inner whitespaces\nHaemorrhoids and Constipated are either `true` or `false`").reply_parameters(ReplyParameters::new(msg.id)).await?;
+            return Ok(());
+        }
+    };
+
+    if failed {
+        bot.send_message(
+            msg.chat.id,
+            "Coudln't insert your shitting session 😥\nPlease report incident to @Mroik",
+        )
+        .reply_parameters(ReplyParameters::new(msg.id))
+        .await?;
+    } else {
+        bot.delete_message(msg.chat.id, msg.id).await?;
     }
     return Ok(());
 }
--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,7 @@
 use crate::DB_NAME;
 use anyhow::Result;
-use rusqlite::Connection;
-use std::{path::Path, sync::Arc};
+use rusqlite::{types::Null, Connection};
+use std::{path::Path, sync::Arc, time::UNIX_EPOCH};
 use teloxide::types::User;
 use tokio::sync::Mutex;
 
@@ -11,7 +11,7 @@ pub async fn setup_db() -> Result<Connection> {
     if !exists {
         conn.execute(
             "CREATE TABLE user (
-                id BIGINT PRIMARY KEY,
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 username TEXT NOT NULL
             )",
             [],
@@ -19,10 +19,10 @@ pub async fn setup_db() -> Result<Connection> {
 
         conn.execute(
             "CREATE TABLE shit_session (
-                id INT PRIMARY KEY,
-                user_id BIGINT NOT NULL,
-                timestamp BIGINT NOT NULL,
-                duration INT,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                timestamp INTEGER NOT NULL,
+                duration INTEGER,
                 location TEXT,
                 haemorrhoids BOOLEAN NOT NULL,
                 constipated BOOLEAN NOT NULL,
@@ -34,19 +34,126 @@ pub async fn setup_db() -> Result<Connection> {
     return Ok(conn);
 }
 
-pub async fn create_or_update_user(
-    conn: Arc<Mutex<Connection>>,
-    user: &User,
-) -> anyhow::Result<()> {
+pub async fn create_or_update_user(conn: Arc<Mutex<Connection>>, user: &User) -> Result<()> {
     let conn = conn.lock().await;
-    let mut statement = conn.prepare(
-        "INSERT INTO user(id, username) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET username = ?",
-    )?;
     let username = if user.username.is_some() {
         user.username.as_ref().unwrap().clone()
     } else {
         user.full_name()
     };
-    statement.execute(rusqlite::params![user.id.0, username, username])?;
+    conn.execute(
+        "INSERT INTO user(id, username) VALUES (?, ?) ON CONFLICT(id) DO UPDATE SET username = ?",
+        rusqlite::params![user.id.0, username, username],
+    )?;
+    return Ok(());
+}
+
+/// With both `duration` and `location` as NULL
+pub async fn insert_shitting_session(
+    conn: Arc<Mutex<Connection>>,
+    user: &User,
+    haemorrhoids: bool,
+    constipated: bool,
+) -> Result<()> {
+    let conn = conn.lock().await;
+    conn.execute(
+        "INSERT INTO shit_session(
+            user_id,
+            timestamp,
+            duration,
+            location,
+            haemorrhoids,
+            constipated
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?
+        )",
+        rusqlite::params![
+            user.id.0,
+            UNIX_EPOCH.elapsed()?.as_secs(),
+            Null,
+            Null,
+            haemorrhoids,
+            constipated
+        ],
+    )?;
+    return Ok(());
+}
+
+// With `location` set as NULL
+pub async fn insert_shitting_session_with_duration(
+    conn: Arc<Mutex<Connection>>,
+    user: &User,
+    duration: u64,
+    haemorrhoids: bool,
+    constipated: bool,
+) -> Result<()> {
+    let conn = conn.lock().await;
+    conn.execute(
+        "INSERT INTO shit_session(
+            user_id,
+            timestamp,
+            duration,
+            location,
+            haemorrhoids,
+            constipated
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?
+        )",
+        rusqlite::params![
+            user.id.0,
+            UNIX_EPOCH.elapsed()?.as_secs(),
+            duration,
+            Null,
+            haemorrhoids,
+            constipated
+        ],
+    )?;
+    return Ok(());
+}
+
+pub async fn insert_shitting_session_with_location(
+    conn: Arc<Mutex<Connection>>,
+    user: &User,
+    duration: u64,
+    location: &str,
+    haemorrhoids: bool,
+    constipated: bool,
+) -> Result<()> {
+    let conn = conn.lock().await;
+    conn.execute(
+        "INSERT INTO shit_session(
+            user_id,
+            timestamp,
+            duration,
+            location,
+            haemorrhoids,
+            constipated
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ?,
+            ?,
+            ?
+        )",
+        rusqlite::params![
+            user.id.0,
+            UNIX_EPOCH.elapsed()?.as_secs(),
+            duration,
+            location,
+            haemorrhoids,
+            constipated
+        ],
+    )?;
     return Ok(());
 }
